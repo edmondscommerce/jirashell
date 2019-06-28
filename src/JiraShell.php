a13e5f3a00e6181ed68f855267ad161ab12f6d04
ben <ben@ben>
2019-06-28 13:23:52 +0000

    Get rid of (most) angry squiggles

--- src/JiraShell.php
+++ src/JiraShell.php
@@ -2,35 +2,32 @@
 
 namespace EdmondsCommerce\JiraShell;
 
-use Exception;
-
 class JiraShell
 {
-    private $jiraUrl = true;
-    private $jiraUser = true;
-    private $jiraPass = true;
+    private $jiraUrl     = true;
+    private $jiraUser    = true;
+    private $jiraPass    = true;
     private $jiraProject = true;
 
     private $queuedIssues = [];
 
     private $queueFilePath;
-    private $envFilePath;
 
-    public function __construct($queueFilePath = null, $envFilePath = null )
+    public function __construct($queueFilePath = null, $envFilePath = null)
     {
-        $this->queueFilePath = ($queueFilePath === null) ?  __DIR__ . '/queue.json' : $queueFilePath;
-        $this->envFilePath = ($envFilePath === null) ? __DIR__ . '/env' : $envFilePath;
+        $this->queueFilePath = ($queueFilePath === null) ? __DIR__ . '/queue.json' : $queueFilePath;
+        $envFilePath         = ($envFilePath === null) ? __DIR__ . '/env' : $envFilePath;
 
-        if (!file_exists($this->envFilePath)) {
-            throw new Exception('env file not found at ' . $this->envFilePath);
+        if (!file_exists($envFilePath)) {
+            throw new \RuntimeException('env file not found at ' . $envFilePath);
         }
-        $env = file($this->envFilePath);
+        $env = file($envFilePath);
         foreach ($env as $item) {
             preg_match('%.*_(.+?)\=(.+?);%', $item, $matches);
             $property = $matches[1];
-            $value = $matches[2];
+            $value    = $matches[2];
             if (!isset($this->$property)) {
-                throw new Exception('invalid env parameter ' . $property);
+                throw new \RuntimeException('invalid env parameter ' . $property);
             }
             $this->$property = $value;
         }
@@ -38,7 +35,7 @@ class JiraShell
             $data = json_encode($this->queuedIssues);
             file_put_contents($this->queueFilePath, $data);
         } else {
-            $queue = file_get_contents($this->queueFilePath);
+            $queue              = file_get_contents($this->queueFilePath);
             $this->queuedIssues = json_decode($queue, true);
         }
     }
@@ -52,22 +49,25 @@ class JiraShell
 
     protected function createIssueData($title, $description, $parent = null)
     {
-        $type = ($parent) ? 'Sub-task' : 'Task';
+        $type = $parent ? 'Sub-task' : 'Task';
+
         $data = [
             'fields' => [
-                'project' => [
-                    'key' => $this->jiraProject
+                'project'     => [
+                    'key' => $this->jiraProject,
                 ],
-                "summary" => "$title",
-                "description" => "$description",
-                "issuetype" => [
-                    "name" => "$type"
-                ]
-            ]
+                'summary'     => (string)$title,
+                'description' => (string)$description,
+                'issuetype'   => [
+                    'name' => $type,
+                ],
+            ],
         ];
+
         if ($parent) {
             $data['fields']['parent']['key'] = $parent;
         }
+
         return $data;
     }
 
@@ -78,7 +78,7 @@ class JiraShell
 
     public function queueIssue($title, $description, array $subtasks = [])
     {
-        $issue = [$title, $description, $subtasks];
+        $issue                = [$title, $description, $subtasks];
         $this->queuedIssues[] = $issue;
     }
 
@@ -93,8 +93,8 @@ class JiraShell
 
     public function createIssue($title, $description, array $subtasks = [], $parent = null)
     {
-        $data = $this->createIssueData($title, $description, $parent);
-        $json = json_encode($data, JSON_FORCE_OBJECT);
+        $data   = $this->createIssueData($title, $description, $parent);
+        $json   = json_encode($data, JSON_FORCE_OBJECT);
         $result = $this->sendIssue($json);
         if ($subtasks) {
             foreach ($subtasks as $subtask) {
@@ -102,6 +102,7 @@ class JiraShell
                 $this->createIssue($subtaskTitle, $subtaskDescription, [], $result->key);
             }
         }
+
         return $result;
     }
 
@@ -123,19 +124,20 @@ class JiraShell
         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        $json = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $json         = curl_exec($ch);
         $error_number = curl_errno($ch);
         if ($error_number > 0) {
-            throw new Exception(
+            throw new \RuntimeException(
                 sprintf('Jira request failed: code = %s, "%s"', $error_number, curl_error($ch))
             );
         }
         curl_close($ch);
         $obj = json_decode($json);
         if (isset($obj->errors)) {
-            throw new Exception('Errors in Jira Respose: ' . print_r($obj));
+            throw new \RuntimeException('Errors in Jira Respose: ' . print_r($obj));
         }
+
         return $obj;
     }
 
@@ -144,26 +146,26 @@ class JiraShell
      */
     public function printQueue()
     {
-        if(!is_array($this->queuedIssues))
-        {
-            echo "No issues found".PHP_EOL;
+        if (!is_array($this->queuedIssues)) {
+            echo 'No issues found' . PHP_EOL;
+
             return;
         }
 
-        echo 'Queued Jira Issues'.PHP_EOL;
-        echo '#########################'.PHP_EOL;
+        echo 'Queued Jira Issues' . PHP_EOL;
+        echo '#########################' . PHP_EOL;
 
-        foreach($this->queuedIssues as $queuedIssue)
-        {
-            echo '-----------------------------------------------------------------------------------------------'.PHP_EOL;
+        foreach ($this->queuedIssues as $queuedIssue) {
+            echo '-----------------------------------------------------------------------------------------------' .
+                 PHP_EOL;
             $this->printIssue($queuedIssue);
         }
     }
 
     public function printIssue(array $task)
     {
-        echo 'Name: '.$task[0].PHP_EOL;
-        echo 'Description: '.$task[1].PHP_EOL;
+        echo 'Name: ' . $task[0] . PHP_EOL;
+        echo 'Description: ' . $task[1] . PHP_EOL;
     }
 }
 
